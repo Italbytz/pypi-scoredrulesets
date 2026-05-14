5efdaa257fe5ca79cab3140a5576c18524ee4fdf
nunkesser <robin.nunkesser@hshl.de>
2026-05-14 09:29:05 +0200

    Export regression prediction API

--- src/scoredrulesets/__init__.py
+++ src/scoredrulesets/__init__.py
@@ -30,8 +30,9 @@ from .estimators.ruleplcs import RulePLCSClassifier
 from .estimators.rulensga2 import RuleNSGA2Classifier
 from .estimators.rulekit_native import RuleKitNativeClassifier
 from .estimators.sklearn_wrapper import ScoredRuleSetClassifier
-from .formatting import format_ruleset_table
-from .io import dump_ruleset_json, load_ruleset_json
+from .formatting import format_ruleset_latex, format_ruleset_markdown, format_ruleset_table
+from .io import dump_ruleset_json, dump_ruleset_latex, dump_ruleset_markdown, load_ruleset_json
+from .runtime import predict_regression
 from .schema import AggregationSpec, Atom, Rule, ScoredRuleSet
 from .importers.logicgp import import_logicgp_json
 
@@ -58,6 +59,8 @@ __all__ = [
     "build_benchmark_leaderboard",
     "compare_benchmark_runs",
     "dump_ruleset_json",
+    "dump_ruleset_latex",
+    "dump_ruleset_markdown",
     "format_benchmark_comparison_report_html",
     "format_benchmark_comparison_report_markdown",
     "format_benchmark_leaderboard_html",
@@ -67,6 +70,8 @@ __all__ = [
     "format_benchmark_meta_report_markdown",
     "format_benchmark_report_html",
     "format_benchmark_report_markdown",
+    "format_ruleset_latex",
+    "format_ruleset_markdown",
     "format_ruleset_table",
     "import_logicgp_json",
     "load_aggregated_results_json",
@@ -74,6 +79,7 @@ __all__ = [
     "plot_benchmark_heatmap",
     "plot_benchmark_heatmap_combined",
     "plot_benchmark_results",
+    "predict_regression",
     "run_benchmarks",
 ]
 
